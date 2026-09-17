3e718c4ed7be0d719497e0134cc76c0c58143f4a
beainoantonio-create <beainoantonio@gmail.com>
2026-09-17 13:16:04 +0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logoImg from '../assets/logo.png';
 import { formatGoogleDriveUrls } from '../utils/googleDrive';
 
 interface LogoProps {
