d8482febcaf24d1b28060cb9d92bf8268ab643ea
beainoantonio-create <beainoantonio@gmail.com>
2026-09-18 11:39:13 +0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,7 @@
+// Vercel automatically turns any file under /api into a serverless function.
+// This one just hands off to the same Express app used everywhere else -
+// see vercel.json for how every /api/* request gets routed here.
+import app from '../server';
+
+export default app;
 
